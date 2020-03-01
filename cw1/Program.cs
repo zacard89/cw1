34af2ba41ec3df953b7985595dabe1fa2e61daf0
Pawel Sobotka <s14144@pjwstk.edu.pl>
2020-03-01 17:47:40 +0100

    zrobiony program do scrapowania. zmiana lokalnego autora

--- cw1/Program.cs
+++ cw1/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           foreach(var a in args)
+            {
+                Console.WriteLine(a);
+            }
+            var emails =await GetEmails(args[0]);
+
+            foreach(var email in emails)
+            {
+                Console.WriteLine(email);
+            }
+                
+        }
+        static async Task<IList<string>> GetEmails(string url)
+        {
+            var HttpClient = new HttpClient();
+            var listOfEmails = new List<string>();
+            var response = await HttpClient.GetAsync(url);
+
+
+
+            Regex emailRegex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", RegexOptions.IgnoreCase);
+
+            MatchCollection emailMatches = emailRegex.Matches(response.Content.ReadAsStringAsync().Result);
+            foreach(var emailMatche in emailMatches)
+            {
+                listOfEmails.Add(emailMatche.ToString());
+            }
+            return listOfEmails;
         }
     }
 }
