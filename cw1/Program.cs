51ddabb799638d9a2745b155a6d08a54c439e927
Piotr Ługowski <s16481@pjwstk.edu.pl>
2020-03-01 17:04:03 +0100

    com1

---
+++ cw1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
